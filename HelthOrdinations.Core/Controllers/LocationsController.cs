1251714770da6c5fd026cb75d8712be0725b3ae8
Misho Spasovski <spasmiso@gmail.com>
2023-08-22 21:52:57 +0200

    Add untracked files

---
+++ HelthOrdinations.Core/Controllers/LocationsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Locations.Core.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LocationsController : ControllerBase
+{    
+    [HttpGet("GetLocations")]
+    public ActionResult<string> GetLocations()
+    {
+        return Ok("asd");
+;    }
+}
